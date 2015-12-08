e2894e29e6d94935f40fbe29323cdc36c29726e3
Avi Samloff <avi.samloff@gmail.com>
2015-12-07 20:46:14 -0800

    (feature) creates team page

--- public/mobile/www/js/controllers.js
+++ public/mobile/www/js/controllers.js
@@ -1,30 +1,33 @@
 angular.module('app.controllers', [])
-  
+
 .controller('registerCtrl', function($scope) {
 
 })
-   
+
 .controller('loginCtrl', function($scope) {
 
 })
-   
+
 .controller('createATeamCtrl', function($scope) {
 
 })
-   
+
 .controller('joinATeamCtrl', function($scope) {
 
 })
-   
+
 .controller('homeCtrl', function($scope) {
 
 })
-   
+
 .controller('rosterCtrl', function($scope) {
 
 })
-   
+
 .controller('gauntletCtrl', function($scope) {
 
 })
- 
+
+.controller('theTeamCtrl', function($scope) {
+
+})
