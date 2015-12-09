cc690332d7334743369685fc175265656d17810a
Kevin Cheng <09chengk@gmail.com>
2015-12-09 15:10:25 -0800

    (feature) builds createATeam controller and createATeam view

--- public/mobile/www/js/services.js
+++ public/mobile/www/js/services.js
@@ -1,10 +1,18 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
+// register factory to sent post request to mongodb to save
+// userName and password
+.factory('PostUsers', ['$http', function($http){
+    return function name(){
+        
+    };
+}])
+
+.factory('PostUsers', [function(){
 
 }])
 
-.service('BlankService', [function(){
+.factory('BlankService', [function(){
 
 }]);
 
