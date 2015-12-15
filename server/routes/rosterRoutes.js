c6ff877ac2a0a7d50f061154099b5075b50ddbf2
Kevin Cheng <09chengk@gmail.com>
2015-12-15 11:34:50 -0800

    (feature) builds roster-controller

--- server/routes/rosterRoutes.js
+++ server/routes/rosterRoutes.js
@@ -4,12 +4,18 @@ var rosterRouter = express.Router();
 
 // Controllers
 var teamCtrl = require('../controllers/team.server.controller');
+var userCtrl = require('../controllers/user.server.controller');
 
 var router = function() {
 
   rosterRouter.route('/roster')
   .post(function (req, res) {
   	teamCtrl.listUsers(req, res);
+  });
+
+  rosterRouter.route('/getTeamName')
+  .post(function (req, res) {
+    userCtrl.listTeams(req, res);
   });
 
   return rosterRouter;
