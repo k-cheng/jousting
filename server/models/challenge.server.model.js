c727f01b0a062a1710bc667c8226400708e2320a
Jeffrey Yang <jeffycyang@gmail.com>
2015-12-15 19:41:07 -0800

    (feature) adds challenge controller

--- server/models/challenge.server.model.js
+++ server/models/challenge.server.model.js
@@ -2,26 +2,35 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var challengeSchema = new Schema({
-    challengeName: 	{	
-    				type: 		String,
-    				trim: 		true,
-			    	required: 	'Please enter a challenge name',
-                    index:      {
-                                    unique: true
-                                }
-			    },
-    team: 		{	
-    				type: 		Schema.Types.ObjectId, 
-    				ref: 		'Team'
-    			},
-    picture: 	{
-    				type: 		String,
-    				trim: 		true
-    			},
-    createdOn: 	{
-    				type: 		Date,
-    				default: 	Date.now
-    			}
+    challengeName: {	
+		type: String,
+		trim: true,
+    	required: 'Please enter a challenge name',
+        index: {
+            unique: true
+        }
+    },
+    team: {	
+		type: Schema.Types.ObjectId, 
+		ref: 'Team'
+	},
+    points: {
+        type: Number,
+        min: 0,
+        required: 'Please enter the number of points this challenge is worth'
+    },
+    usersCompleted: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    picture: {
+		type: String,
+		trim: true
+	},
+    createdOn: {
+		type: Date,
+		default: Date.now
+	}
 });
 
 module.exports = mongoose.model( 'Challenge', challengeSchema );
--- server/models/team.server.model.js
+++ server/models/team.server.model.js
@@ -2,28 +2,32 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var teamSchema = new Schema({
-    teamName: 	{	
-    				type: 		String,
-    				trim: 		true,
-    				unique: 	true,
-			    	required: 	'Please enter a team name'
-			    },
-    createdBy: 	{
-    				type: 		Schema.Types.ObjectId,
-    				ref: 		'User'
-    			},
-    users: 		[{
-    				type: 		Schema.Types.ObjectId,
-    				ref: 		'User'
-    			}],
-    picture: 	{
-    				type: 		String,
-    				trim: 		true
-    			},
-    createdOn: 	{
-    				type: 		Date,
-    				default: 	Date.now
-    			}
+    teamName: {	
+		type: String,
+		trim: true,
+		unique: true,
+    	required: 'Please enter a team name'
+	},
+    createdBy: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	},
+    users: [{
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	}],
+    challenges: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Challenge'
+    }],
+    picture: {
+		type: String,
+		trim: true
+	},
+    createdOn: {
+		type: Date,
+		default: Date.now
+	}
 });
 
 module.exports = mongoose.model( 'Team', teamSchema );
