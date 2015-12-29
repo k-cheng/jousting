4230c86f552b62d5bc84fb29cb71d0663663d37d
Cory Dang <corydang@Corys-MacBook-Pro.local>
2015-12-29 12:33:56 -0800

    (fix) fixes controllers

--- public/mobile/www/js/controllers/createteam.js
+++ public/mobile/www/js/controllers/createteam.js
@@ -1,5 +1,9 @@
 angular.module('app').controller('CreateTeamCtrl', function($scope, $http, API_URL, $state) {
+<<<<<<< 301c983e161fec8f6682806e72a4eacc3c2d8acb
   var userName = window.localStorage.userName;
+=======
+  var userName = JSON.parse(window.localStorage.userName);
+>>>>>>> (fix) fixes controllers
   var teams = window.localStorage.teams;
   console.log('userNode team is ', teams);
   console.log('scope user is', userName);
